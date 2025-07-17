49acea8e3fa37c7f82e84c077b1cf21e06b50d81
Eric Smekens <ericsmekens@msn.com>
2025-07-17 21:25:24 +0200

    Backend update cors, remove tests

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Enable CORS for all origins by default
+  app.enableCors({
+    origin: ['https://oidis.netlify.app', 'http://localhost:3000'], // your frontend domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   await app.listen(process.env.PORT ?? 8080);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
