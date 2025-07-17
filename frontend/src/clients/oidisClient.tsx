eec4bb4a52bc4a20e2f9bd047f5fbb9e1a280cd4
Eric Smekens <ericsmekens@msn.com>
2025-07-17 21:13:36 +0200

    Move away from mongodb data api

--- frontend/src/clients/oidisClient.tsx
+++ frontend/src/clients/oidisClient.tsx
@@ -1,42 +1,18 @@
-const dataBaseUri = "https://eu-central-1.aws.data.mongodb-api.com/app/oidis-backend-cmimcek/endpoint/data/v1/action";
-const authUri = "https://eu-central-1.aws.services.cloud.mongodb.com/api/client/v2.0/app/oidis-backend-cmimcek/auth/providers/anon-user/login";
+const apiUri = "https://service-name-300347239104.europe-west1.run.app";
 
-const authorize = async () => {
-    const response = await fetch(authUri,
+const fetchAllRecipes = async () => {
+    const response = await fetch(`${apiUri}/recipes`,
         {
-            method: "post",
+            method: "get",
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             }
-        }
-    );
-
-    const responseBody = await response.json();
-    return responseBody.access_token;
-}
-
-const fetchAllRecipes = async (authToken: string) => {
-    const response = await fetch(`${dataBaseUri}/find`,
-        {
-            method: "post",
-            headers: {
-                'Authorization': `Bearer ${authToken}`,
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                "dataSource": "mongodb-atlas",
-                "database": "oidis",
-                "collection": "recipes",
-                "filter": { }
-            })
         });
 
-    return (await response.json()).documents;
+    return (await response.json());
 }
 
 export const fetchRecipes = async () => {
-    const authToken = await authorize();
-    return fetchAllRecipes(authToken);
+    return fetchAllRecipes();
 };
